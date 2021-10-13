b3c08a7dba842a304db565464ed6948bb1008dd0
TruongNBN <truongnbn.20185418@sis.hust.edu.vn>
2021-10-13 10:17:34 +0700

    add .gitingnore file

---
+++ src/math/Matrix.cs
@@ -0,0 +1,19 @@
+namespace math {
+    public class Matrix {
+        // Constructor
+
+        // Initialize
+
+        // Properties
+        public int Row { get; set; } = 0;
+        public int Col { get; set; } = 0;
+        public long[,] Value { set; } = new long[Row,Col];
+
+        // Getter
+
+        // Setter
+
+        // Operator
+
+    }
+}
