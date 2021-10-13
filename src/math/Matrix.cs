587cb074b5c83801eb234043856bc7ecd1ed5e69
TruongNBN <truongnbn.20185418@sis.hust.edu.vn>
2021-10-13 11:28:54 +0700

    delete foder .vs add this into .gitignore

--- src/math/Matrix.cs
+++ src/math/Matrix.cs
@@ -1,13 +1,20 @@
 namespace math {
     public class Matrix {
         // Constructor
-
+        public Matrix() {
+            Row = 0;
+            Col = 0;
+        }
+        public Matrix(int row = 0, int col = 0) {
+            Row = row;
+            Col = col;
+        }
         // Initialize
 
         // Properties
         public int Row { get; set; } = 0;
         public int Col { get; set; } = 0;
-        public long[,] Value { set; } = new long[Row,Col];
+        // public long[,] Value { set; } = new long[0,0];
 
         // Getter
 
