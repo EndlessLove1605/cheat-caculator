2321f4b2aa950625f93c85859d9621ee50ea4be6
TruongNBN <truongnbn.20185418@sis.hust.edu.vn>
2021-10-13 09:08:22 +0700

    add dotnet core to run in VS Code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using number;
 
-namespace CheatCaculator
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace CheatCaculator {
+	class Program {
+		static void Main(string[] args) {
 			double d = .6;
 
 			Console.WriteLine($"check = {new Fraction(d)}");
